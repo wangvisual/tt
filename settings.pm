9171ed68736cda7f6aa3721865c90a52a8bcda94
Opera Wang <Opera.Wang+step@gmail.com>
2019-09-11 08:36:08 -0700

    revert 	modified:   settings.pm

--- settings.pm
+++ settings.pm
@@ -5,15 +5,15 @@ use warnings;
 use utf8;
 
 $settings::debug = 1;
-$settings::mail = 'weiw@synopsys.com';
+$settings::mail = 1;
 
 $settings::ldapserver = "ldap";
 $settings::name = 'displayName';
 $settings::email = 'mail';
 $settings::employeeNumber = 'employeeNumber';
-$settings::baseDN = 'ou=people,o=synopsys.com'; # Change this to the real baseDN
+$settings::baseDN = 'ou=people'; # Change this to the real baseDN
 
-$settings::servername = "peweb"; # Change this to the web server host name
-$settings::title = '乒乓球比赛与积分系统测试版';
+$settings::servername = ""; # Change this to the web server host name
+$settings::title = '乒乓球比赛与积分系统';
 
 1;
