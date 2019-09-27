21c87949d396fa17f8619d40f0cf805501b2e17a
Opera Wang <Opera.Wang+step@gmail.com>
2019-09-27 08:31:48 -0700

    add more links 	modified:   more.js 	modified:   tt.css

--- more.js
+++ more.js
@@ -1,1 +1,34 @@
-more = '';
+more = `
+<div id='more'>
+    <div>积分:</br>
+        <a target='_blank' href='http://cntt.sports.cn/sshg/2014hyls/tzgg/2014-08-03/2349855.html'>介绍</a>
+        <a target='_blank' href='http://xhimg.sports.cn/Image/soft/170621/10-1F621100540936.doc'>计算方法</a>
+    </div>
+
+    <div>横拍教学:</br>
+        <a target='_blank' href='http://www.pingpangwang.com/qmxpp-1-0-0-0-0-0-0-1-0-1.html'>论坛</a>
+        <a target='_blank' href='https://www.youtube.com/watch?v=vG4V5NTzRm0&list=PLYbRscEONpFGda3zMvzXtZ0fSu_wVSi1N'>Youtube</a>
+        </br>
+        <a target='_blank' href='https://pan.baidu.com/s/1ThDlX3cUCeWrcB6Kcxs9WA'>提取码:372g</a>
+    </div>
+
+    <div>直拍教学:</br>
+        <a target='_blank' href='http://www.pingpangwang.com/qmxpp-0-0-0-0-0-0-0-2-0-1.html'>论坛</a>
+        <a target='_blank' href='https://www.youtube.com/playlist?list=PLYbRscEONpFFOautAg6AqRuo1P7oL7xks'>Youtube</a>
+    </div>
+
+    <div>更多视频下载:</br>
+        <a target='_blank' href='https://pan.baidu.com/s/1So_jnAp8oh8V2-9LDDJ4hA'>提取码: cw3b</a>
+    </div>
+
+    <div>
+        2019年大循环赛:</br>
+        <a target='_blank' href='https://www.meipian.cn/2b6f4xn9'>上篇</a>
+        </br>
+        <a target='_blank' href='https://www.meipian.cn/2csqliz8'>中篇</a>
+        </br>
+        <a target='_blank' href='https://www.meipian.cn/2dv1z5sn'>视频</a>
+    </div>
+
+</div>
+`;
